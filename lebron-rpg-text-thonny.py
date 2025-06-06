ed93162554f5ef54e2121d72254ebd64c8645760
ysukumar123 <yadevs723@deltalearns.ca>
2025-06-06 14:26:47 -0700

    Initial Basic Commit
    Just added the greetings when u start and some of the basic structure for the game.

---
+++ lebron-rpg-text-thonny.py
@@ -0,0 +1,6 @@
+#Text-Based RPG: Lebron Simulator
+# Ask User for their name and greet the user.
+name = input("Tell me your Name Bum")
+print(f"Who would Name their kid{name}? Doesn't matter your gonna be Lebron anyways.")
+
+# Start the Game
