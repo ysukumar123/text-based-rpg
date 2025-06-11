26f1731c00273ac1e57dee5adc7603966b09432f
ysukumar123 <yadevs723@deltalearns.ca>
2025-06-11 10:35:06 -0700

    small edtis
    no time to describe

--- lebron-rpg-text-thonny.py
+++ lebron-rpg-text-thonny.py
@@ -1,6 +1,8 @@
 #Text-Based RPG: Lebron Simulator
+print("Life of Lebron Simulator")
+
 # Ask User for their name and greet the user.
-name = input("Tell me your Name Bum")
+name = input("Tell me your Name Bum").lower().strip()
 print(f"Who would Name their kid{name}? Doesn't matter your gonna be Lebron anyways.")
 
 # Start the Game
